64a1e78ed469b21e6e5a216999f2e087d0b2bc87
Don Mallory <dmm333@gmail.com>
2020-02-22 02:47:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_list)
+  dwarf_list.each_with_index{|dwarf, index} puts dwarf
 end
 
 def summon_captain_planet# code an argument here
